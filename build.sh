be478b0731c320c0370546aa37be055b02d25447
Soaicl-as <social.marketing638@gmail.com>
2025-03-10 17:11:20 -0700

    build.sh

--- build.sh
+++ build.sh
@@ -9,55 +9,48 @@ apt-get update
 
 # Install Google Chrome
 echo "Installing Google Chrome..."
-wget -qO- https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > chrome.deb
-apt-get install -y ./chrome.deb
-rm -f chrome.deb  # Clean up
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+apt-get update
+apt-get install -y google-chrome-stable
 
-# Install ChromeDriver (matching the installed Chrome version)
+# Get Chrome version and install matching ChromeDriver
 echo "Installing ChromeDriver..."
-CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
-CHROMEDRIVER_VERSION=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)
-wget -qO- https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip > chromedriver.zip
-unzip chromedriver.zip -d /usr/local/bin/
-rm -f chromedriver.zip  # Clean up
+CHROME_VERSION=$(google-chrome --version | cut -d ' ' -f 3 | cut -d '.' -f 1)
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget -q -O /tmp/chromedriver.zip "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+unzip -q /tmp/chromedriver.zip -d /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
+rm /tmp/chromedriver.zip
 
-# Install dependencies for Chrome and Selenium
+# Install additional dependencies
 echo "Installing dependencies..."
 apt-get install -y \
     libxss1 \
-    libappindicator3-1 \
-    libindicator7 \
-    fonts-liberation \
-    libnss3 \
-    libgdk-pixbuf2.0-0 \
+    libgbm1 \
     libasound2 \
-    libx11-xcb1 \
-    libxcomposite1 \
-    libxrandr2 \
     libgtk-3-0 \
-    libdbus-glib-1-2 \
-    libnspr4 \
-    libxtst6 \
+    libx11-xcb1 \
+    xdg-utils \
     lsb-release \
-    wget \
-    unzip
+    fonts-liberation \
+    libappindicator3-1 \
+    libnspr4 \
+    libnss3 \
+    xvfb
+
+# Setup Xvfb for headless browser
+echo "Setting up Xvfb..."
+Xvfb :99 -screen 0 1280x1024x24 > /dev/null 2>&1 &
+echo "export DISPLAY=:99" >> ~/.bashrc
 
 # Upgrade pip to the latest version
 echo "Upgrading pip..."
-pip install --upgrade pip
+pip install --upgrade pip wheel setuptools
 
-# Install Selenium explicitly
-echo "Installing Selenium..."
-pip install selenium==4.29.0
-
-# Install the rest of the requirements from requirements.txt
-if [ -f "requirements.txt" ]; then
-    echo "Installing requirements from requirements.txt..."
-    pip install -r requirements.txt
-else
-    echo "requirements.txt not found. Skipping..."
-fi
+# Install Python requirements
+echo "Installing Python requirements..."
+pip install -r requirements.txt
 
 # Make start.sh executable
 echo "Making start.sh executable..."
